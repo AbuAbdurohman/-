0a3a39ef60ee536c28e7655e293da2cc041b61e0
AbuAbdurrohman <kambaralyabytaliev6@gmail.com>
2024-02-08 15:05:27 +0600

    initial

---
+++ main.js
@@ -0,0 +1,26 @@
+let a = prompt('напиши число до 12')
+		if (a == 1) {
+			alert('январь')
+		}else if (a == 2) {
+			alert('февраль')
+		}else if (a == 3) {
+			alert('март')
+		}else if (a == 4) {
+			alert('апрель')
+		}else if (a == 5) {
+			alert('май')
+		}else if (a == 6) {
+			alert('июнь')
+		}else if (a == 7) {
+			alert('июль')
+		}else if (a == 8) {
+			alert('август')
+		}else if (a == 9) {
+			alert('сентябрь')
+		}else if (a == 10) {
+			alert('октябрь')
+		}else if (a == 11) {
+			alert('ноябрь')
+		}else if (a == 12) {
+			alert('декабрь')
+		}
